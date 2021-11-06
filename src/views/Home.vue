99c97ae0578fd02c1c034dfc0326338373f5ec9a
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2021-11-06 21:01:22 +0100

    changed starting page layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container p-5 text-center">
-  <div class="h2 mb-4">Choose sports</div>
-  <router-link class="btn btn-primary px-4 py-3" to="/climbing">Climbing</router-link>
+  <div class="h2 mb-4 fw-bold">Choose sport</div>
+  <router-link class="btn btn-primary px-4 py-3" to="/climbing">Climbing (beta)</router-link>
 </div>
 
 </template>
